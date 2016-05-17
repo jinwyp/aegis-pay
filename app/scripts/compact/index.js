33ac283c4eaf2bd896205c49147e618917707fae
beatacao <beatacao@lotus.local>
2016-05-17 13:35:25 +0800

    add lightbox

--- app/scripts/compact/index.js
+++ app/scripts/compact/index.js
@@ -1,27 +1,33 @@
 requirejs.config({
   baseUrl: 'scripts',
-  components: '../components',
-  libs: '../libs',
   paths: {
-    flexslider: 'components/flexslider/jquery.flexslider-min',
-    jquery: 'libs/jquery-2.2.3.min'
+    components: '../components',
+    libs: '../libs',
+    flexslider: '../components/flexslider/jquery.flexslider-min',
+    jquery: '../libs/jquery-2.2.3.min',
+    lightbox: '../components/lightbox2/dist/js/lightbox.min'
   },
 	shim: {
-		'jquery': {
-			exports: 'jQuery'
-		},
 		'flexslider': {
 			deps: ['jquery'],
 			exports: 'flexslider'
+		},
+    'lightbox': {
+			deps: ['jquery'],
+			exports: 'lightbox'
 		}
 	}
 });
-require(['common', 'jquery', 'flexslider'], function(common, $, flexslider){
-  $('.slides').css('width', $('.slides .item').width()*$('.slides .item').size() + 'px')
+require(['common', 'jquery', 'flexslider', 'lightbox'], function(common, $, flexslider, lightbox){
+  $('.slides').css('width', ($('.slides .item').width()+10)*$('.slides .item').size() + 'px')
   $('.flexslider').flexslider({
     animation: "slide",
     animationLoop: false,
     itemWidth: 210,
     itemMargin: 5
   });
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  })
 })
