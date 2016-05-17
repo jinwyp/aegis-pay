7a0c9097ec2aeb7400bb585bcfb9dd9a8dc84ba5
chenluily0451 <chenluily@163.com>
2016-05-17 09:32:37 +0800

    head foot

--- nock/header.js
+++ nock/header.js
@@ -4,9 +4,8 @@ var API = require('../api/v1/api_config');
 var compact = nock(API.host);
 
 compact
-.get('/compact?orderid=1&action=get').reply(200, {
-  "success": true,
-  "compact":'/Users/beatacao/work/aegis-pay/servicestatic/compact.html'
-})
+    .get('/header').reply(200, {
+      "success": true
+    })
 
 module.exports = compact;
