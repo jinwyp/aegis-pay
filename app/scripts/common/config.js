002ec18cc80c59bc0897b8dbc1ef482a0d19fdbe
beatacao <beatacao@lotus.local>
2016-05-19 12:21:36 +0800

    add bower-requirejs

---
+++ app/scripts/common/config.js
@@ -0,0 +1,22 @@
+requirejs.config({
+  baseUrl: 'scripts',
+  paths: {
+    'blueimp-file-upload': '../../components/blueimp-file-upload/js/jquery.fileupload',
+    'blueimp-canvas-to-blob': '../../components/blueimp-canvas-to-blob/js/canvas-to-blob',
+    'blueimp-load-image': '../../components/blueimp-load-image/index',
+    'blueimp-tmpl': '../../components/blueimp-tmpl/js/tmpl',
+    jquery: '../../components/jquery/dist/jquery',
+    bootstrap: '../../components/bootstrap/dist/js/bootstrap',
+    'eonasdan-bootstrap-datetimepicker': '../../components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min',
+    moment: '../../components/moment/moment',
+    'moment-timezone': '../../components/moment-timezone/builds/moment-timezone-with-data-2010-2020',
+    flexslider: '../../components/flexslider/jquery.flexslider',
+    lightbox2: '../../components/lightbox2/dist/js/lightbox'
+  },
+  packages: [
+
+  ],
+  shim: {
+
+  }
+})
