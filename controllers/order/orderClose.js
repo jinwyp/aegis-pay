1100053829d660458f8822097c1accae80e219c5
tianjiliu <tian@141421>
2016-05-23 16:12:38 +0800

    提交 关闭订单

--- controllers/order/orderClose.js
+++ controllers/order/orderClose.js
@@ -1,9 +1,10 @@
 /*
+* 关闭订单页面
 * 业务控制 (模板 & 数据请求)
 * */
 
 var request = require('request');
-var apiHost = 'http://server.180.com/';			// 模拟域名
+var apiHost = 'http://server.180.com/';			// API域名
 
 
 // 订单信息
@@ -13,42 +14,8 @@ exports.orderInfo = function (req, res, next) {
 	request(apiHost + 'infoObj', function (err, data) {
 
 
-
-		// 订单状态 数据模拟
-		var statusObj = {
-			step: 4,        // 第几步
-			stepList: [
-				{
-					stepName: '提交订单',
-					stepDate: '2016-05-11 01:02:03'
-				},
-				{
-					stepName: '签订合同',
-					stepDate: '2016-05-12 01:02:03'
-				},
-				{
-					stepName: '付款',
-					stepDate: '2016-05-13 01:02:03'
-				},
-				{
-					stepName: '确认提货',
-					stepDate: '2016-05-14 01:02:03'
-				},
-				{
-					stepName: '结算',
-					stepDate: '2016-05-15 01:02:03'
-				}
-			]
-		};
-
-
-		var DATA = {
-			infoObj: JSON.parse(data.body),
-			statusObj: statusObj
-		}
-
-
-		res.render('order/orderClose', DATA);			// 渲染页面,指定模板&数据
+		var replyData = JSON.parse(data.body);
+		res.render('order/orderClose', replyData);			// 渲染页面,指定模板&数据
 
 	});
 
--- web_router.js
+++ web_router.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express');
-var demoController = require('./controllers/demo');                 // 引入 控制模块
+var demoController = require('./controllers/demo');                             // 引入 控制模块
 var siteController = require('./controllers/site');
 // var staticController = require('./controllers/static');
 // var signController = require('./controllers/sign');
@@ -18,7 +18,7 @@ var orderCloseContr = require('./controllers/order/orderClose');                
 
 
 var router = express.Router();
-router.get('/demo', demoController.demo);                           // 添加路由
+router.get('/demo', demoController.demo);                                       // 添加路由
 router.get('/', siteController.home);
 // router.post('/signout', signController.signout);
 // router.post('/signin', signController.signin);
@@ -28,7 +28,7 @@ router.get('/subHeader', subHeaderController.subHeader);
 router.get('/footer', footerController.footer);
 router.get('/confirmDelivery', confirmDeliveryController.confirmDelivery);
 router.get('/getOrderDetail', orderController.getOrderDetail);
-router.get('/order/orderClose', orderCloseContr.orderInfo);                   // 关闭订单 路由
+router.get('/order/orderClose', orderCloseContr.orderInfo);                     // 关闭订单 路由
 
 
 
