eb8761bada3b1ff00dfb608ac564355990de75c8
hary <94093146@qq.com>
2016-05-26 18:15:16 +0800

    updated

--- backend/run.sh
+++ backend/run.sh
@@ -5,9 +5,13 @@ cd $script_dir;
 
 export NODE_PATH=$HOME/.nvm/versions/node/`node -v`/lib/node_modules;
 export DOMAIN=cn;
-export MOCK=false;
 export DEBUG=true;
 export MODE=local;
+export MOCK=false;
+
+if [[ $# = 1 ]]; then
+    export MOCK=true;
+fi
 
 supervisor -w bin,common,config,models,nock,routes,views,app.js app.js 
 
