a1eb451540e561cf6f72ca78dbdbbcca8f6d6554
hary <94093146@qq.com>
2016-05-31 11:47:27 +0800

    update make debug.sh

--- debug.sh
+++ debug.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
 script_dir=$(cd `dirname $0`; pwd);
+source $script_dir/../aegis-docker/bin/aegis-config;
+source $script_dir/../aegis-docker/bin/aegis-common.sh;
 mysql_dir=$(cd $script_dir/../aegis-docker/docker-mysql; pwd);
 redis_dir=$(cd $script_dir/../aegis-docker/pay-redis; pwd);
 nginx_dir=$(cd $script_dir/../aegis-docker/docker-nginx; pwd);
 service_dir=$(cd $script_dir/../aegis-service; pwd);
-
-source $script_dir/../aegis-docker/bin/aegis-config;
-source $script_dir/../aegis-docker/bin/aegis-common.sh;
 
 eval $(docker-machine env testing);
 
 nginx_ip=`get_vm_ip`;
 
 # 启动基础镜像
-start_docker pay-redis $redis_dir;
-start_docker mysql $mysql_dir;
-start_docker aegis-service  $service_dir;
-start_docker nginx $nginx_dir;
-
-# 如果aegis-pay-dev镜像存在, 则删除
-if docker ps -a | grep aegis-pay-dev > /dev/null 2>&1; then
-	if docker ps | grep aegis-pay-dev > /dev/null 2>&1; then
-		docker stop aegis-pay-dev;
-	fi
-	docker rm aegis-pay-dev;
-fi
+cd $redis_dir;    make start;
+cd $mysql_dir;    make start;
+cd $service_dir;  make start;
+cd $nginx_dir;    make start;
+cd $script_dir;   make clean;
 
 # 启动测试
-echo "启动测试...";
 cd $script_dir;
+echo "启动测试...";
 mkdir -p files;
 docker run -it --rm \
   --name aegis-pay \
--- make
+++ make
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+source ../aegis-docker/bin/aegis-config;
 export container_name=aegis-pay-dev;
 export project_name=aegis-pay;
 export image_name=aegis-pay;
 export create_param="";
-export ip=10.0.20.2
+export ip=${aegis_pay_ip};
 export build_type=node;
 export registry=registry.yimei180.com;
 export dev_registry=192.168.99.100:5000; 
