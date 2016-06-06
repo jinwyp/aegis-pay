d8e0def508047f632a05c7fda05a64d3d352f8cf
JinWYP <jinwyp@gmail.com>
2016-06-06 16:33:47 +0800

    update checker

--- backend/common/datachecker.js
+++ backend/common/datachecker.js
@@ -11,11 +11,13 @@ function isFunction(fn) {
 }
 
 
-function throwError (err, isNext){
+function throwError (code, message, field, isNext){
+    field = field || '';
+
     if (isFunction(isNext)) {
-        return isNext(err);
+        return isNext(new ValidationError(code, message, field));
     }else {
-        throw(err);
+        throw(new ValidationError(code, message, field));
     }
 }
 
@@ -24,22 +26,23 @@ function throwError (err, isNext){
 
 exports.orderId = function(orderId, next){
     if (!orderId || !validator.isLength(orderId, { min: 6, max: 100}) || !validator.isInt(orderId, { min: 100000, max: 90000000}) ) {
-        return throwError(new ValidationError(ValidationError.code.order.orderIdWrong, 'Field validation error, orderId length should be 6 - 100', 'orderId'), next);
+        return throwError(ValidationError.code.order.orderIdWrong, 'Field validation error, orderId length should be 6 - 100', 'orderId', next);
     }
 };
 
 exports.captchaType = function(captchaType, next){
     if (!captchaType || typeof captchaType !== 'string' || !validator.isLength(captchaType, { min: 2, max: 50}) ) {
-        return throwError(new ValidationError(ValidationError.code.captcha.typeWrong, 'Field validation error, captcha type length should be 2 - 50', 'captchaType'), next);
+        return throwError(ValidationError.code.captcha.typeWrong, 'Field validation error, captcha type length should be 2 - 50', 'captchaType', next);
     }
 };
 
 exports.captchaText = function(captchaText, next){
     if (!captchaText || typeof captchaText !== 'string' || !validator.isLength(captchaText, { min: 2, max: 10}) ) {
-        return throwError(new ValidationError(ValidationError.code.captcha.textWrong, 'Field validation error, captcha text length should be 2 - 10', 'captchaText'), next);
+        return throwError(ValidationError.code.captcha.textWrong, 'Field validation error, captcha text length should be 2 - 10', 'captchaText', next);
     }
 };
 
 exports.captchaNotMatch = function(next){
-    return throwError(new ValidationError(ValidationError.code.captcha.notMatch, 'Field validation error, captcha text not match', 'captchaText'), next);
+    return throwError(ValidationError.code.captcha.notMatch, 'Field validation error, captcha text not match', 'captchaText', next);
 };
+
