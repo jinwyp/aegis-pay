9b93c391f2985d649c2bc2180b33b9d7280adb1e
JinWYP <jinwyp@gmail.com>
2016-06-08 17:06:20 +0800

    删除 eventproxy 库

--- backend/web_router.js
+++ backend/web_router.js
@@ -13,7 +13,6 @@ var authMiddleware = require('./middlewares/auth');
 var demoController = require('./controllers/demo');                                         // 引入 控制模块
 var siteController = require('./controllers/site');
 // var staticController = require('./controllers/static');
-// var signController = require('./controllers/sign');
 var compactController         = require('./controllers/compact');
 var headerController          = require('./controllers/header');
 var footerController          = require('./controllers/footer');
@@ -32,8 +31,7 @@ var signCtrl = require('./controllers/sign');                                   
 
 router.get('/demo', demoController.demo);                                                   // 添加路由
 router.get('/', siteController.home);
-// router.post('/signout', signController.signout);
-// router.post('/signin', signController.signin);
+
 
 router.get('/header', headerController.header);
 router.get('/subHeader', subHeaderController.subHeader);
