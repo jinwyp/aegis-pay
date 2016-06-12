d94498fbec82dd4e5b13b93401236ec6e037d51b
JinWYP <jinwyp@gmail.com>
2016-06-12 16:21:18 +0800

    update

--- backend/controllers/disputeComplete.js
+++ backend/controllers/disputeComplete.js
@@ -2,13 +2,13 @@
  * 业务控制 (模板 & 数据请求)
  * */
 
-var request    = require('request');
+var request = require('request');
 
 var api_config = require('../api/v1/api_config');
-var logger = require("../libs/logger");
+var logger     = require("../libs/logger");
 
 exports.disputeComplete = function (req, res, next) {
-    
+
     var url = api_config.disputeComplete;
     request({url : url}, function (err, data) {
 
@@ -17,11 +17,11 @@ exports.disputeComplete = function (req, res, next) {
         logger.debug('获取到的错误是----------------------------' + err);
         logger.debug('获取到的结果是data----------------------------' + data.body);
 
-        if (data){
+        if (data) {
             var source  = JSON.parse(data.body);
             var content = {
-                headerTit   : "纠纷申请完成",
-                pageTitle   : "纠纷申请完成"
+                headerTit : "纠纷申请完成",
+                pageTitle : "纠纷申请完成"
             };
             logger.debug('获取到的结果是content----------------------------' + content);
             //渲染页面,指定模板&数据
