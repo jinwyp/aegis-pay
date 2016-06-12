1f225504f5ef28fe8a6bd48618403df25d941783
tianjiliu <tian@141421>
2016-06-12 20:11:18 +0800

    结算单迭代更新

--- backend/web_router.js
+++ backend/web_router.js
@@ -18,16 +18,16 @@ var footerController          = require('./controllers/footer');
 var orderController           = require('./controllers/order/orderDetail');
 var confirmDeliveryController = require('./controllers/confirmDelivery');
 var subHeaderController       = require('./controllers/subHeader');
-var orderCloseContr           = require('./controllers/order/orderClose');                            // 关闭订单 模块(控制文件路径)
-var settlementFormContr       = require('./controllers/settlement/settlementForm');               // 结算单开具页面 模块(控制文件路径)
-var sellerDeliveryController  = require('./controllers/sellerDelivery');                     // 关闭订单 模块(控制文件路径)
+var orderCloseContr           = require('./controllers/order/orderClose');                      // 关闭订单 模块(控制文件路径)
+var settlementFormContr       = require('./controllers/settlement/settlementForm');             // 结算单开具页面 模块(控制文件路径)
+var sellerDeliveryController  = require('./controllers/sellerDelivery');                        // 关闭订单 模块(控制文件路径)
 var returnDetailController    = require('./controllers/returnDetail');
-var confirmComplete           =require('./controllers/confirmComplete');            //确认完成页面
-var payCtl                    = require('./controllers/pay');                                                  //支付模块
+var confirmComplete           =require('./controllers/confirmComplete');                        //确认完成页面
+var payCtl                    = require('./controllers/pay');                                   //支付模块
 
 
 
-router.get('/demo', demoController.demo);                                                   // 添加路由
+router.get('/demo', demoController.demo);                                                       // 添加路由
 router.get('/', siteController.home);
 // router.post('/signout', signController.signout);
 // router.post('/signin', signController.signin);
@@ -40,11 +40,10 @@ router.get('/confirmDelivery/test', confirmDeliveryController.test);
 router.get('/getOrderDetail', orderController.getOrderDetail);
 router.get('/orderTest', orderController.orderTest);
 router.get('/printDetail', orderController.printDetail);
-router.get('/order/orderClose', orderCloseContr.orderInfo);                                 // 关闭 订单路由
-router.get('/settlement/settlementForm_buyer', settlementFormContr.orderInfo);              // 结算单 买家 页面路由
-router.get('/settlement/settlementForm_seller', settlementFormContr.orderInfo);             // 结算单 卖家 页面路由
+router.get('/order/orderClose', orderCloseContr.orderInfo);                                     // 关闭 订单路由
+router.get('/settlement/settlementForm', settlementFormContr.orderSettlement);                  // 结算单 买家 页面路由
 router.get('/confirmDelivery/sellerDelivery', sellerDeliveryController.sellerDelivery);
-router.get('/confirmDelivery/confirmComplete', confirmComplete.confirmComplete);    //确认完成页面
+router.get('/confirmDelivery/confirmComplete', confirmComplete.confirmComplete);                //确认完成页面
 
 
 router.get('/return', returnDetailController.returnDetail);
