95e9aa9a50742811bffae426a29c118cd1692eb4
tianjiliu <tian@141421>
2016-06-13 15:11:35 +0800

    订单状态条 功能扩展

--- backend/nock/demo.js
+++ backend/nock/demo.js
@@ -4,7 +4,7 @@
 
 
 var nock = require('nock');
-var listData = nock('http://server.180.com/');		// 需要替换的请求域名
+var listData = nock('http://server.180.com/').persist();		// 需要替换的请求域名
 
 listData
 	.get('/pageTitle').reply(200, {
@@ -14,10 +14,10 @@ listData
 	})
 
 	.get('/listData').reply(200, [
-		{"id":"A01", "name":"北京1", "desc":"app desc11"},
-		{"id":"A02", "name":"上海2", "desc":"app desc22"},
-		{"id":"A03", "name":"杭州3", "desc":"app desc33"}
-	])
+		{"id":"A01", "name":"北京1", "desc":"app desc91"},
+		{"id":"A02", "name":"上海2", "desc":"app desc92"},
+		{"id":"A03", "name":"杭州3", "desc":"app desc93"}
+	]);
 
 
 module.exports = listData;
