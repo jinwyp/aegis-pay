d08985bf2bed132e025fa1b3f0dcb74cd055eca4
liushengbin512 <liushengbin@zrworldwide.com>
2016-06-13 16:36:58 +0800

    修改二级头部问题

--- backend/controllers/wealth/financialCenter.js
+++ backend/controllers/wealth/financialCenter.js
@@ -5,24 +5,13 @@
 
 var request = require('request');
 var api_config = require('../../api/v1/api_config');
+var logger     = require("../../libs/logger");
 
 
 // 处理业务逻辑
 exports.financialCenter = function (req, res, next) {
-    var firstTab=req.query.firstTab;
-    var secondTab=req.query.;
-    //if(firstTab==null){
-    //    //homePage,accountPage,transactionPage,settlementPage,contractPage
-    //    firstTab="accountPage";
-    //    secondTab=1;
-    //}else{
-    //    firstTab=firstTab;
-    //    if(firstTab=="accountPage"){
-    //        secondTab=1;
-    //    }else{
-    //
-    //    }
-    //}
+    var firstTab=req.query.firstTab==undefined?"homePage":req.query.firstTab;
+    var secondTab=req.query.secondTab==undefined?1:req.query.secondTab;
     var content = {
         pageTitle : "财务管理中心",
         headerTit : "财务管理中心",
@@ -32,7 +21,6 @@ exports.financialCenter = function (req, res, next) {
         }
     };
     //渲染页面
-    //res.render('global/header/financialCenterHeader',content);
     res.render('wealth/financialCenter',content);
 };
 
