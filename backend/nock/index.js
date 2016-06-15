a8f6de0f89d18050e9df682bcd246b93483a12c6
liushengbin512 <liushengbin@zrworldwide.com>
2016-06-15 19:00:28 +0800

    提交交易记录的页面样式

--- backend/nock/index.js
+++ backend/nock/index.js
@@ -6,6 +6,7 @@ require('./order/orderClose');                      // 关闭订单
 require('./settlement/settlementForm');             // 开具结算单
 require('./settlement/confirmTheInvoice');          // 结算.确认开票信息
 require('./confirmDelivery/confirmComplete');
+require('./confirmDelivery/confirmDelivery');
 require('./pay'); //支付
 require('./return/returnDetail');
 require('./dispute/disputeApply');
