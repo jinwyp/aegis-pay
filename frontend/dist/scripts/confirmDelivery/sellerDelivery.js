70902c22eed73b1f1fce3c60c12fb677bf6885f4
JinWYP <jinwyp@gmail.com>
2016-06-17 19:15:47 +0800

    更新css

--- frontend/dist/scripts/confirmDelivery/sellerDelivery.js
+++
@@ -1,7 +0,0 @@
-require(['jquery', 'bootstrap'],function($, upload){
-    $("#textInput").on("input",function(){
-        var Len=$(this).val().length;
-        var restNum=200-Len;
-        $("#restNum").text(restNum)
-    });
-});
