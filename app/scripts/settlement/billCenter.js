1a00b233830d1295dfb4a62779903b4d79f02d92
Daisy-dan <wudan@yimei180.com>
2016-06-17 11:27:03 +0800

    modify scss

--- app/scripts/settlement/billCenter.js
+++ app/scripts/settlement/billCenter.js
@@ -9,6 +9,7 @@ require(['jquery','bootstrap'],function($){
         $(".modal_1").modal();
         $("#modalImg_1").addClass("yes").removeClass("attention");
         $("#modalInfo_1").html("短信通知买家开票?");
+        $("#modalInfo_1").css({fontSize:"16px"});
     });
     $("#md_ok_1").click(function(){
         location.href="/"
