fc95a3fb10744c632077dd796d98adc6dfc7c724
JinWYP <jinwyp@gmail.com>
2016-06-17 13:51:19 +0800

    update

--- docs/swagger/index.js
+++ docs/swagger/index.js
@@ -2,9 +2,8 @@ var bp = require('bootprint').load(require('bootprint-openapi'));  // Load bootp
 
 var mergeOptions = {};
 
-
 bp.merge(mergeOptions) // Customize configuration, override any options
-    .build('./source/finalcial.json', './output')// Specify build source and target
+    .build('./source/financial.json', './output')// Specify build source and target
     .generate() // Generate swagger-documentation into "output" directory
     .done(console.log);
 
