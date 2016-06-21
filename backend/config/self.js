2778a17a41a71041a109f582a9997b4a91644f65
Daisy-dan <wudan@yimei180.com>
2016-06-21 09:31:33 +0800

    update

--- backend/config/self.js
+++ backend/config/self.js
@@ -5,17 +5,18 @@
 var path = require('path');
 
 var config = {
-    port     : 3001, // 程序运行的端口
+    port     : 3003, // 程序运行的端口
     domain   : 'pay-local.yimei180.com', // 域名
-    homepage : 'http://pay-local.yimei180.com:3001',
+    homepage : 'http://pay-local.yimei180.com:3003',
 
     files_root : path.resolve(process.env.FILES_DIR + '/..'), // 上传文件的临时目录
     passport   : {
         member     : 'http://member-local.yimei180.com:3000',
         cookieName : 'passport'
     },
-    member_address: 'http://127.0.0.1:3000',
-    rest_address: '123456'
+    member_address: 'http://127.0.0.1:3003',
+    rest_address: '',
+    mock:false
 };
 
 module.exports = config;
