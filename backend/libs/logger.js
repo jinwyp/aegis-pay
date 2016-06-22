2246ce341c93e62160807006c68707a9e970db97
hongpengfei <hongpengfei@yimei180.com>
2016-06-22 18:34:25 +0800

    1

--- backend/libs/logger.js
+++ backend/libs/logger.js
@@ -14,9 +14,6 @@ var filenameDebug = config.logdir +  env+ '-debug.log';
 // var filenameInfo = config.logdir + env + '-info.log';
 var filenameError = config.logdir + env + '-error.log';
 
-console.log(filenameDebug);
-console.log(filenameError);
-console.log(pathLog);
 
 utils.makeDir(pathLog);
 
