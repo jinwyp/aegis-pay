335a63e81ac3004a5dabf463ce81949edd949c3c
Daisy-dan <wudan@yimei180.com>
2016-06-22 17:56:57 +0800

    update

--- backend/controllers/settlement/billSetting.js
+++ backend/controllers/settlement/billSetting.js
@@ -13,47 +13,52 @@ var api_config = require('../../api/v1/api_config');
 // 处理业务逻辑
 exports.billSetting = function (req, res, next) {
 
-
-    var userId = req.session.user.id;
-    //头部
-    var firstTab=req.query.firstTab==undefined?4:req.query.firstTab;
-    var secondTab=req.query.secondTab==undefined?2:req.query.secondTab;
-
-    var accountSideBar = {
-        current : "2",
-        secCurrent:'',
-        sideBarList : [
-            {
-                listName : '发票管理',
-                listLink : 'javascript:void(0);',
-                secList  :[
-                    {
-                        secListName:"全部发票",
-                        secListLink:"billCenter"
-                    },
-                    {
-                        secListName:'待开票' ,
-                        secListLink:"waitSettle"
-                    },
-                    {
-                        secListName:'已开票',
-                        secListLink:"hadSettle"
-                    }
-                ]
-            },
-            {
-                listName : '开票设置',
-                listLink : 'billSetting',
-                secList:''
-            }
-        ]
-    };
-
     request({url : api_config.billSetting+"?userId="+15}, function (err, data) {
+
+        var userId = req.session.user.id;
+        var source = JSON.parse(data.body);
+        //头部
+        var firstTab=req.query.firstTab==undefined?4:req.query.firstTab;
+        var secondTab=req.query.secondTab==undefined?2:req.query.secondTab;
+
+        var accountSideBar = {
+            current : "2",
+            secCurrent:'',
+            sideBarList : [
+                {
+                    listName : '发票管理',
+                    listLink : 'javascript:void(0);',
+                    secList  :[
+                        {
+                            secListName:"全部发票",
+                            secListLink:"billCenter",
+                            secListNum:source.data.receiptOrder.count
+                        },
+                        {
+                            secListName:'待开票' ,
+                            secListLink:"waitSettle",
+                            secListNum:source.data.receiptOrder.waitCount
+
+                        },
+                        {
+                            secListName:'已开票',
+                            secListLink:"hadSettle",
+                            secListNum:source.data.receiptOrder.openCount
+                        }
+                    ]
+                },
+                {
+                    listName : '开票设置',
+                    listLink : 'billSetting',
+                    secList:''
+                }
+            ]
+        };
+
         if (err) return next(err);
 
         if(data) {
-            var source = JSON.parse(data.body);
+
             var content = {
                 pageTitle: "结算管理",
                 headerTit: "结算管理",
@@ -62,8 +67,6 @@ exports.billSetting = function (req, res, next) {
                     secondTab: secondTab
                 },
                 accountSideBar: accountSideBar,
-                waitSettleNum: 8,
-                hadSettleNum: 4,
                 receipt:source.data.receipt
 
             };
