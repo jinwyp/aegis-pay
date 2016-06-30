2cab55c9ad52f6808088decb2d2b81f49e5c0dab
Daisy-dan <wudan@yimei180.com>
2016-06-30 17:54:36 +0800

    update wealthCenter

--- frontend/scripts/wealth/wealthCenter.js
+++ frontend/scripts/wealth/wealthCenter.js
@@ -8,50 +8,37 @@ require(['jquery','bootstrap', 'message'],function($, bootstrap, message){
 
     // 点击.未认证弹出框
     $("#open-btn").click(function(){
-        $.post({
-            url:    '/api/wealth/checkUserCompany',
-            success: function(data){
-                if(data.success) {
-
-                        location.href = '/wealth/open-fund-account';
-
-                } else {
-                    // setTimeout(function() {
-                        $(".modal_2").modal();
-                        $("#modalInfo_2").html("贵公司尚未通过认证，无法开通资金账户");
-                        $("#md_ok_2").val("前往认证公司信息");
-                    // }, 800);
-                }
-            }
-        });
-    });
-
-
-    // 点击.前往认证公司
-    $("#md_ok_2").click(function(){
         var memberUrl = $("#memberUrl").val();
-
         $.post({
             url:    '/api/wealth/checkUserCompany',
             data:   {userId:$("#userId").val()},
-            // type: 'json',
             success: function(data){
                 if(data.success) {
-
+                    location.href = '/wealth/open-fund-account';
                 } else {
                     if(data.error == "verifying"){                            //待审核
-                        location.href=memberUrl+" /account/companyDetail";     //查看页面
+                        $(".modal_3").modal();
+                        $("#modalInfo_3").html("贵公司企业认证正在审核中，无法开通资金账户");
                     }else if(data.error == "verifyNotPass"){                  //审核未通过
-                        location.href=memberUrl+" /account/companyDetail";      //查看页面
-                    }else if(data.error.indexOf("companyNotExists") !== -1){      //审核未通过
-                        location.href=memberUrl+"/account/companyCreate";       //新建页面
-                    }
+                        $(".modal_2").modal();
+                        $("#modalInfo_2").html("贵公司尚未通过认证，无法开通资金账户");
+                        $("#md_ok_2").val("前往认证公司信息");
+                        $("#md_ok_2").click(function(){
+                            location.href=memberUrl+" /account/companyDetail";
+                        });
+                    }else if(data.error  == "companyNotExists"){              //未认证
+                        $(".modal_2").modal();
+                        $("#modalInfo_2").html("贵公司尚未通过认证，无法开通资金账户");
+                        $("#md_ok_2").val("前往认证公司信息");
+                        $("#md_ok_2").click(function(){
+                            location.href=memberUrl+"/account/companyCreate";
+                        })
 
+                    }
 
                 }
             }
         });
-
     });
 
 
