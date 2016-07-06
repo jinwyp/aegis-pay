941874afc58e3a6e3458d4279929f1539d965c34
JinWYP <jinwyp@gmail.com>
2016-07-06 14:35:41 +0800

    update

--- backend/libs/request.js
+++ backend/libs/request.js
@@ -1,4 +1,4 @@
-var rq = require('request');
+var request = require('request');
 var _ = require('lodash');
 
 var PageNotFoundError = require('../errors/PageNotFoundError');
@@ -14,6 +14,10 @@ function isObject(obj) {
     return obj === Object(obj);
 }
 
+function isFunction(functionToCheck) {
+    var getType = {};
+    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+}
 
 // organize params for post get
 function initParams(uri, options, callback){
@@ -70,7 +74,50 @@ function initParams(uri, options, callback){
     return params;
 }
 
+/*
+*  request('http://www.google.com', function (error, response, body){})
+*  request('http://google.com/doodle.png').pipe(fs.createWriteStream('doodle.png'))
+*  request.post('http://service.com/upload', {form:{key:'value'}})
+*  request.post('http://service.com/upload').form({key:'value'})
+*  request.post( {url:'http://service.com/upload', form: {key:'value'}}, function(err,httpResponse,body){})
+*
+*/
 
+var callbackWithErrorHandler = function(callback){
+
+    return function(err, response, body){
+        if (err){
+            return callback(err, response, body);
+        }else if (response.statusCode === 200){
+            return callback(err, response, body);
+        }else{
+            var tempBody = {};
+            if (isObject(response.body)){
+                tempBody = response.body;
+            }else {
+                tempBody = JSON.parse(response.body);
+            }
+
+            var errMessage = tempBody.error + ". Url: " + tempBody.path + ". Reason: " + tempBody.message + ". Exception: " + tempBody.exception;
+
+            if (response.statusCode === 400 || response.statusCode === 405) { // 参数错误
+                return callback(new MethodArgumentNotValidError(400, errMessage) )
+
+            }else if (response.statusCode === 401) { // 重新登录
+                return callback(new UnauthenticatedAccessError(401, errMessage) )
+
+            }else if (response.statusCode === 403) { // 没有权限访问
+                return callback(new UnauthorizedAccessError(403, errMessage) )
+
+            }else if (response.statusCode === 409) { // 业务逻辑错误
+                return callback(new BusinessError(409, errMessage) )
+
+            }else {
+                return callback(new SystemError(500, errMessage, err) )
+            }
+        }
+    }
+};
 
 
 function verbFunc (verb) {
@@ -78,16 +125,16 @@ function verbFunc (verb) {
     return function (uri, options, callback) {
         var params = initParams(uri, options, callback)
         params.method = method;
-        return rq(params, params.callback)
+        return request(params, params.callback)
     }
 }
 
-function request (uri, options, callback) {
+function requestWithErrorHandler (uri, options, callback) {
     var params = initParams(uri, options, callback)
-    return rq(params);
+    return request(params);
 }
 
-request.post = verbFunc('post');
-request.get = verbFunc('get');
+requestWithErrorHandler.post = verbFunc('post');
+requestWithErrorHandler.get = verbFunc('get');
 
-module.exports =request;
+module.exports = requestWithErrorHandler;
